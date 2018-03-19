9cd3825d57b9e7fb09015ffa533269061110ac45
Jennifer Elton <justjenny40@hotmail.com>
2018-03-19 11:22:32 -0700

    completed reducers and reducer tests for goals

--- src/components/dashboard/reducers.js
+++ src/components/dashboard/reducers.js
@@ -1,12 +1,21 @@
 export const ADD_GOAL = 'ADD_GOAL';
+export const DELETE_GOAL = 'DELETE_GOAL';
+export const COMPLETE_GOAL = 'COMPLETE_GOAL';
 
-export function goal(state = [], { type, payload }) {
+export function goals(state = [], { type, payload }) {
   switch(type) {
     case ADD_GOAL:
       return [
         ...state,
         payload
       ];
+
+    case DELETE_GOAL:
+      return state.filter(g => g.id !== payload);
+
+    case COMPLETE_GOAL:
+      return state.map(g => g.id === payload.id ? payload : g);
+
     default: 
       return state;
   }
--- src/components/dashboard/reducers.test.js
+++ src/components/dashboard/reducers.test.js
@@ -1,7 +1,44 @@
-import { goal, ADD_GOAL } from './reducers';
+import { goals, ADD_GOAL, DELETE_GOAL, COMPLETE_GOAL } from './reducers';
 
 
 it('default state of an empty array', () => {
-  const state = goal(undefined, {});
+  const state = goals(undefined, {});
   expect(state).toEqual([]);
+});
+
+it('adds a goal', () => {
+  const state = goals([], { 
+    type: ADD_GOAL, 
+    payload: { name: 'goal' }
+  });
+  expect(state).toEqual([{ name: 'goal' }]);
+});
+
+it ('deletes a goal', () => {
+  const state = goals([{ id: 123 }], {
+    type: DELETE_GOAL,
+    payload: 123,
+  });
+  expect(state).toEqual([]);
+});
+
+const futureGoals = [{
+  id: 123,
+  name: 'pizza',
+  completed: false
+}];
+
+const crushedGoal = {
+  id: 123,
+  name: 'pizza',
+  completed: true,
+  date: '3/19/18'
+};
+
+it('completes a goal', () => {
+  const state = goals(futureGoals, { 
+    type: COMPLETE_GOAL,
+    payload: crushedGoal
+  });
+  expect(state).toEqual([crushedGoal]);
 });
