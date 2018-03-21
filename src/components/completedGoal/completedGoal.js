21f399b6478c5e3a2066acb166044ce011b308ef
Jennifer Elton <justjenny40@hotmail.com>
2018-03-21 14:27:46 -0700

    base CompletedGoal component

---
+++ src/components/completedGoal/completedGoal.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+class CompletedGoal extends PureComponent {
+
+  render() {
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default connect (
+  null
+)(CompletedGoal);
