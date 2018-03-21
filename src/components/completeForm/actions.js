fd99f5192cbf70ebb79915992e0284ec80068d16
Jennifer Elton <justjenny40@hotmail.com>
2018-03-20 17:00:17 -0700

    added action to add completed goals

--- src/components/completeForm/actions.js
+++ src/components/completeForm/actions.js
@@ -1,10 +1,14 @@
 import { db } from '../../services/firebase';
 
 const users = db.ref('users');
+const shared = db.ref('shared');
 
 export function addCompletedGoal(goal) {
   return (dispatch, getState) => {
     let { uid } = getState().user;
     users.child(uid).child('completedGoals').push(goal);
+    if(goal.share) shared.push(goal);
   };
 }
+
+
