b29db66c16368cd4cba5ac9cf46fbbd27d25c966
mewarren <holla@mewarren.com>
2018-03-22 15:37:25 -0700

    shared gallery mobile css

--- src/components/sharedGallery/SharedGallery.js
+++ src/components/sharedGallery/SharedGallery.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './sharedGallery.css';
 
 class SharedGallery extends PureComponent {
 
@@ -10,7 +11,14 @@ class SharedGallery extends PureComponent {
       <ul>
         {sharedGoals.map((goal, i) => {
           return <li key={i}>
-            <Link to={`/goal/${goal.id}`}>{goal.name}</Link>
+            <Link to={`/goal/${goal.id}`}>
+              <article className="galleryImage">
+                <img src={goal.image}/>
+                <div className="headline">
+                  <h2>{goal.name}</h2>
+                </div>
+              </article>
+            </Link>
           </li>;
         })}
       </ul>
