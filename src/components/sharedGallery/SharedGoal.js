f66bc29970fc5629a20f2be3bc84404db315ea97
mewarren <holla@mewarren.com>
2018-03-21 19:28:34 -0700

    add profile link in sharedGoal

--- src/components/sharedGallery/SharedGoal.js
+++ src/components/sharedGallery/SharedGoal.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class SharedGoal extends PureComponent {
 
@@ -7,7 +8,12 @@ class SharedGoal extends PureComponent {
     const { sharedGoals, id } = this.props;
     const sharedGoal = sharedGoals.find(goal => goal.id === id);
     return (
-      <h2>Youve Reach Shared goal {sharedGoal.name}</h2>
+      <div>
+        <h2>Youve Reach Shared goal {sharedGoal.name}</h2>
+        <h3>
+          <Link to={`/profile/${sharedGoal.uid}`}>{sharedGoal.user}</Link>
+        </h3>
+      </div>
     );
   }
 }
