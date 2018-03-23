6aab5e2c8b695287fb81898cf2c2f08553b1f78c
Jennifer Elton <justjenny40@hotmail.com>
2018-03-22 18:07:08 -0700

    oh man home page looks good

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,9 +12,10 @@ class Home extends PureComponent {
       <div className="auth container">
         <div className="image-container">
           <Auth history={this.props.history}/>
+          <p className="text">We are a place to help you realize your dreams.<br/>A place to find a path...</p>
+         
   
         </div>
-        <div className="overlay"><p>sample text yadda yadda</p></div>
       </div>
     );
   }
