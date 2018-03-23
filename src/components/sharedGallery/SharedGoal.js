087e8078edddbe1604c203e2710eca08b0b071a6
mewarren <holla@mewarren.com>
2018-03-22 17:45:24 -0700

    sharedGoal css

--- src/components/sharedGallery/SharedGoal.js
+++ src/components/sharedGallery/SharedGoal.js
@@ -1,18 +1,38 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './sharedGoal.css';
+import { getSharedGoals } from '../explore/actions';
 
 class SharedGoal extends PureComponent {
 
+  componentDidMount() {
+    this.props.getSharedGoals();
+  }
+  
   render() {
     const { sharedGoals, id } = this.props;
     const sharedGoal = sharedGoals.find(goal => goal.id === id);
+
+    if(!sharedGoal) return null;
+
     return (
-      <div>
-        <h2>Youve Reach Shared goal {sharedGoal.name}</h2>
-        <h3>
-          <Link to={`/profile/${sharedGoal.uid}`}>{sharedGoal.user}</Link>
-        </h3>
+      <div className="sgoal-detail">
+        <img src={sharedGoal.image}/>
+        {/* <div className="detail-flex"> */}
+          <h3>
+            <Link to={`/profile/${sharedGoal.uid}`}>{sharedGoal.user}</Link>
+          </h3>
+          <h2>{sharedGoal.name}</h2>
+        {/* </div> */}
+        <div className="detail-flex">
+          <h4>{sharedGoal.date}</h4>
+          <h4>|</h4>
+          <h4>{sharedGoal.location}</h4>
+        </div>
+        <p>{sharedGoal.description}</p>
+
+        
       </div>
     );
   }
@@ -22,5 +42,5 @@ export default connect((state, props) => ({
   sharedGoals: state.sharedGoals,
   id: props.match.params.id
 }), 
-null
+{ getSharedGoals }
 )(SharedGoal);
